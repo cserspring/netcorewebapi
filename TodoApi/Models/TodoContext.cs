9f97c73d02b77ae8e2c8d63fe69b9c7631648a6e
cserspring <shuaiwhu@gmail.com>
2018-06-19 22:45:56 -0700

    A Sample .NET Core Web Api Project

---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿namespace TodoApi.Models
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
